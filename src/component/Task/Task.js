c0b1289f7d544ecdae13cc6c0b84beb15776f9cc
prajktamhase <mhaseprajkta@gmail.com>
2023-09-22 19:13:54 +0800

    app edited

--- src/component/Task/Task.js
+++ src/component/Task/Task.js
@@ -1,11 +1,17 @@
 import React from "react";
 import "./Task.css"
- const Task = ({id,title,description,priority}) => {
+ const Task = ({id,title,description,priority,removeTaskFromList,setTaskEditable}) => {
     return(
         <div className="task-container">
             <h1 className="task-title">{title}</h1>
             <p className="task-description">{description}</p>
            <span className="task-priority"> 🎯{priority}</span>
+           <span className="task-delete-icon" onClick={() => {removeTaskFromList(id)}}>
+            🚮</span>
+            <span className="task-edit-icon" onClick={() =>{
+                setTaskEditable(id);
+            }}>
+            🖋️</span>
 
 
         </div>
--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,37 +1,83 @@
-import react from 'react';
-import Task from "./../../component/Task/Task";
+import React, { useEffect } from 'react';
+import Task from './../../component/Task/Task';
 import './Home.css';
 
 import { useState } from 'react';
+
 const Home = () => {
     const [taskList, setTaskList] = useState([
         {
             id: 1,
             title: 'submit',
-            description: "jhfgdzs",
+            description: "Assignment jaldi submit karo",
             priority: 'High',
         },
+    ]);
+    const [id,setId]=useState(0);
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+    const [priority, setPriority] = useState('');
+    const [isEdit,setIsEdit]=useState(true);
 
-    ])
+    useEffect(() => {
+        const list = JSON.parse(localStorage.getItem('getShitDone'));
+        if (list && list.length>0) {
+            setTaskList(list);
+        }
+    }, []);
 
-    const [ title, setTitle ] = useState('');
-    const [ description, setDescription ] = useState('');
-    const [ priority, setPriority ] = useState('');
-    const addTaskToList=() => {
-        const randomId=Math.floor(Math.random() *1000);
+    const saveListToLocalStorage = (tasks) => {
+        localStorage.setItem('getShitDone', JSON.stringify(tasks));
+    };
+
+    const addTaskToList = () => {
+        const randomId = Math.floor(Math.random() * 1000);
 
         const obj = {
-        id: randomId,
-        title: title,
-        description: description,
-        priority: priority,
-    }
-        setTaskList([...taskList,obj])
-        
+            id: randomId,
+            title: title,
+            description: description,
+            priority: priority,
+        };
+
+        const newTaskList = [...taskList, obj];
+
+        setTaskList(newTaskList);
+
         setTitle('');
         setDescription('');
-        setPriority('');  
-}  
+        setPriority('');
+
+        saveListToLocalStorage(newTaskList);
+    };
+
+    const removeTaskFromList = (id) => {
+        let index;
+        taskList.forEach((task,i)=>{
+            if(task.id===id){
+            index=i
+           }
+        })
+
+    const tempArray=taskList;
+    tempArray.splice(index,1);
+
+    setTaskList([...tempArray])
+    saveListToLocalStorage(tempArray)
+    }
+    const setTaskEditable=(id)=> {
+     setIsEdit(true);
+     setId(id);
+     let currentEditTask;
+
+     taskList.forEach((task) => {
+        if(task.id===id){
+            currentEditTask=task; 
+        }
+     })
+     console.log(currentEditTask)
+    
+    }
 
     return (
         <>
@@ -41,61 +87,85 @@ const Home = () => {
                 <div className='flex-container'>
                     <div>
                         <h2 className='center-text'>Show List</h2>
-                        {
-                            taskList.map((taskItem, index) => {
-                                const { id, title, description, priority } = taskItem;
+                        {taskList?.map((taskItem, index) => {
+                            const { id, title, description, priority } = taskItem;
 
-                                return (<Task id={id} title={title} description={description} priority={priority} />
-                                )
-
-                            })
-                        }
+                            return (
+                                <Task
+                                    id={id}
+                                    title={title}
+                                    description={description}
+                                    priority={priority}
+                                    key={index}
+                                    removeTaskFromList={removeTaskFromList}
+                                    setTaskEditable={setTaskEditable}
+                                />
+                            );
+                        })}
                     </div>
 
                     <div>
-                        <h2 className='center-text'> Add List</h2>
+                        <h2 className='center-text'> 
+                        {isEdit ? `Update Task ${id}` : 'Add Task'}
+                        </h2>
                         <div className='add-task-from-container'>
-                            <h3>Show Title:{title}</h3>
+                            <h3>Show Title: {title}</h3>
                             <form>
-                                
-                                <input type='text' 
-                                value={title}
-                                 onChange={(e) => {
-                                setTitle(e.target.value)
-                                console.log(e)
-                                }} 
-                                placeholder='Enter Title'
-                                className='task-container'/>
+                                <input
+                                    type='text'
+                                    value={title}
+                                    onChange={(e) => {
+                                        setTitle(e.target.value);
+                                    }}
+                                    placeholder='Enter Title'
+                                    className='task-container'
+                                />
 
+                                <input
+                                    type='text'
+                                    value={description}
+                                    onChange={(e) => {
+                                        setDescription(e.target.value);
+                                    }}
+                                    placeholder='Enter Description'
+                                    className='task-container'
+                                />
 
-                                <input type='text' 
-                                value={description} 
-                                onChange={(e) => {
-                                setDescription(e.target.value)
-                                console.log(e)
-                                }} 
-                                placeholder='Enter Description'
-                                className='task-container'/>
-
-
-                                <input type='text' 
-                                value={priority} 
-                                onChange={(e) => {
-                                setPriority(e.target.value)
-                                console.log(e)
-                                }} 
-                                placeholder='Enter Priority'
-                                className='task-container'/>
-                             
-                        <button type='button' className='add-button' onClick={addTaskToList}>Add Task To List</button>
+                                <input
+                                    type='text'
+                                    value={priority}
+                                    onChange={(e) => {
+                                        setPriority(e.target.value);
+                                    }}
+                                    placeholder='Enter Priority'
+                                    className='task-container'
+                                />
+                                <div className='btn-container'>
+                                    {isEdit ?
+                                     <button
+                                     type='button'
+                                     className='add-button'
+                                     onClick={addTaskToList}
+                                 >
+                                     Update
+                                 </button>
+                                 :
+                                 <button
+                                 type='button'
+                                 className='add-button'
+                                 onClick={addTaskToList}
+                             >
+                                 Add 
+                             </button>
+                                 }
+                                 </div>
                             </form>
-
                         </div>
                     </div>
                 </div>
             </div>
-
         </>
-    )
-}
-export default Home
+    );
+};
+
+export default Home;
